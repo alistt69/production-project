248e674767dc6c574d165acbdbd4959972306153
Artyom Kachyro <artyomkachyro2006@gmail.com>
2024-12-18 17:50:21 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,8 +6,7 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-
-    return [
+    const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html,
         }),
@@ -19,9 +18,17 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        })
-    ]
+    ];
+
+    if (isDev) {
+        plugins.push(
+            new ReactRefreshWebpackPlugin(),
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false
+            })
+        );
+    }
+
+
+    return plugins;
 }
