435f8760e47daa9d8ccd7461ecc2035bff1b575d
Artyom Kachyro <artyomkachyro2006@gmail.com>
2025-04-17 19:07:55 +0300

    fix scss lint

--- src/features/auth-by-username/ui/login-modal/index.tsx
+++ src/features/auth-by-username/ui/login-modal/index.tsx
@@ -1,5 +1,3 @@
-import { classNames } from "shared/lib/classes";
-import classes from "./classes.module.scss";
 import { Modal } from "shared/ui/modal";
 import { LoginForm } from "../login-form";
 
@@ -11,7 +9,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
     return (
-        <Modal className={classNames(classes.login_modal, {}, [className])} isOpen={isOpen} onClose={onClose} lazy>
+        <Modal isOpen={isOpen} onClose={onClose} lazy>
             <LoginForm />
         </Modal>
     );
