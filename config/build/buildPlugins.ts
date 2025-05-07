ebd1e64f9ab32d6b63ce95bf055442dba0947b2e
Artyom Kachyro <artyomkachyro2006@gmail.com>
2025-05-07 19:12:27 +0300

    replace BundleAnalyzerPlugin to dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,16 +18,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    })
   ];
-
-
 
   if (isDev) {
     plugins.push(
       new ReactRefreshWebpackPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
     );
   }
 
